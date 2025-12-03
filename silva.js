5d365aa3fa966edc715b7d97332b68a7792c59cc
Silva Tech Nexus <silvatechnexus@gmail.com>
2025-12-03 08:38:27 +0300

    Refactor QR route and update console log message

--- silva.js
+++ silva.js
@@ -1,5 +1,3 @@
-// silva.js
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -7,25 +5,19 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 
 let code = require('./pair');
-let qr = require('./qr');   // ⭐ NEW QR ROUTE
+let qr = require('./qr');
 
-require('events').EventEmitter.defaultMaxListeners = 500;
-
-// ROUTES
 app.use('/code', code);
-app.use('/qr', qr);  // ⭐ NEW QR API
+app.use('/qr', qr);
 
-// Serve 'main.html' as default
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main.html'));
 });
 
-// Serve pair page
 app.get('/pair', (req, res) => {
   res.sendFile(path.join(__dirname, 'pair.html'));
 });
 
-// Serve QR display page
 app.get('/scan', (req, res) => {
   res.sendFile(path.join(__dirname, 'qr.html'));
 });
@@ -34,10 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-  console.log(`
-Deployment Successful!
-
-Silva-Session-Server Running on http://localhost:` + PORT)
+  console.log(`Silva QR + Pair Server running on ${PORT}`);
 });
 
 module.exports = app;
